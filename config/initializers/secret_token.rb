ab41708a8d702a5c619960bb523500745c177b42
Ayal Dvir <aldr04@gmail.com>
2013-08-13 05:23:32 -0700

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstAyalApp::Application.config.secret_key_base = '2f9f0afb59954b072e918316dcf7320bfd9e45de559994d2b6de5d1a18ad663eb17e0e88b3a2e263ebad6111b60b6707ecee8b3f0806dd74546c988c02bd46e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstAyalApp::Application.config.session_store :cookie_store, key: '_firstAyalApp_session'
